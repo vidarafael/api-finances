3dc87a146a64705249070abcfece3b9b76fc1ef5
Rafael Freitas <vidarafael@hotmail.com>
2022-08-06 08:28:32 -0300

    feature: middleware of authenticate for wallet route

--- src/modules/wallets/infra/http/routes/wallets.routes.ts
+++ src/modules/wallets/infra/http/routes/wallets.routes.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
+import { ensureAuthenticated } from "../../../../../shared/infra/http/middlewares/ensureAuthenticated";
 import { WalletsController } from "../controller/WalletsController";
 
 const walletRoute = Router()
 const walletsController = new WalletsController()
+
+walletRoute.use(ensureAuthenticated)
 
 walletRoute.post('/', walletsController.create)
 walletRoute.get('/', walletsController.find)
